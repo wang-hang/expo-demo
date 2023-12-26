07ee6c10ff402600088af5dc4af637a980ef569b
wanghang <wanghang@htx-inc.com>
2023-12-26 11:59:28 +0800

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { useEffect } from "react"
+import { StyleSheet, Text, View } from "react-native"
+import { Asset } from "expo-asset"
 
 export default function App() {
+  useEffect(() => {
+    const asset = Asset.fromModule(require("./assets/a.txt"))
+    console.log("file path:", asset.localUri)
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+})
